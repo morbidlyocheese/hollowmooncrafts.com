6418439d6ce271fca6103a33493a8d81ec377857
damien <warwickjunglex@gmail.com>
2023-08-08 18:22:01 -0400

    added vue

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <HomePage class="page-container"/>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue';
+
+export default {
+    name: 'App',
+    components: {
+        HomePage,
+        // other links
+    }
+}
+
+</script>
+
+<style>
+<!-- html {} -->
+
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    <!-- text-align: center;
+    margin-top: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column; -->
+}
+
+<!-- .page-container {} -->
+
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="page-container">
+        <div class="content-container">
+            <div class="content">
+                <h1 class="name">under construction</h1>
+            </div>
+        </div>
+    </div>
+</template>
